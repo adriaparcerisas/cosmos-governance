044f7c10d2a0b0ccdbc6040b91520d8960213845
Adrià Parcerisas <adria.parcerisas.albes@gmail.com>
2023-01-23 12:08:55 +0100

    Update 4. Axelar Governance Activity.py

--- pages/4. Axelar Governance Activity.py
+++ pages/4. Axelar Governance Activity.py
@@ -385,9 +385,10 @@ group by 1 order by 1 asc
 
 
 st.experimental_memo(ttl=21600)
+@st.cache
 def compute(a):
-    data=sdk.query(a)
-    return data
+    result=sdk.query(a)
+    return result
 
 results = compute(sql)
 df = pd.DataFrame(results.records)
